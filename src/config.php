607ab88b6ca42ff0f4e88b6700fcf6e125f41e8e
Marc Moral <marcmllll2001@gmail.com>
2020-11-25 16:15:00 +0100

    config fet

--- src/config.php
+++ src/config.php
@@ -1,1 +1,18 @@
-Moral
+<?php
+
+$config = array();
+
+/* configuració de connexió a la base dades */
+$config["db"] = array();
+$config["db"]["user"] = 'citaprevia';
+$config["db"]["pass"] = '1234';
+$config["db"]["dbname"] = 'cita_previa';
+$config["db"]["host"] = 'localhost';
+
+
+/* Nom de la cookie */
+$config["cookie"]["name"] = 'tasques';
+
+require_once "../src/models/cita-model.php";
+require_once "../src/models/festius-model.php";
+require_once "../src/models/usuario-model.php";
